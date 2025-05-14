3e6812763a7594b8b1fd8bee5354016f3173209a
gonza <gonza@Gonzalo>
2025-05-14 12:06:14 +0200

    empleados

--- ProjectoGrupal_Entornos/src/concesionario/empleados.java
+++ ProjectoGrupal_Entornos/src/concesionario/empleados.java
@@ -1,5 +1,24 @@
 package concesionario;
 
 public class empleados {
+	
+	
+		    private String nombre;
+		    private String dni;
+		    private int tamaño;
+
+		    public empleados() {
+		        nombre = "";
+		        dni = "";
+		        tamaño = 0;
+		    }
+
+		    // Constructor con parámetros
+		    public empleados(String nombre, String dni, int tamaño) {
+		        this.nombre = nombre;
+		        this.dni = dni;
+		        this.tamaño = tamaño;
+		    }
+
 
 }
